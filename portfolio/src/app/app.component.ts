9af6b11739669bccc1176c6b51788abf09f23e6f
Jing Xiong <jxiong@berkeley.edu>
2018-01-29 23:53:20 -0800

    added reactive header

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { Component, ViewChild, ElementRef, Renderer2, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild(HeaderComponent, {read: ElementRef}) headerComponent: ElementRef;
+
+  constructor(private renderer: Renderer2) {
+  }
+  ngAfterViewInit() {
+    this.renderer.listen(window, "scroll", () => {
+      console.log(document.scrollingElement.scrollTop);
+      this.renderer.setStyle(this.headerComponent.nativeElement, 'height', 'calc(6vh - ' + document.scrollingElement.scrollTop + 'px)');
+    });
+  }
 }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
 import { HeaderComponent } from './header/header.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
 
+const appRoutes: Routes = [
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'about', component: AboutComponent},
+  { path: '',   redirectTo: '/portfolio', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    PortfolioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
